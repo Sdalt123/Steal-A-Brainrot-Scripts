e4d9df89202297e6188a91e572b7f81e772f71e9
Sdalt123 <yeniposta071@outlook.com>
2026-03-01 19:31:06 +0300

    Update fly_open_in_executor.lua

--- fly_open_in_executor.lua
+++ fly_open_in_executor.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sdalt123/Steal-A-Brainrot-Scripts/main/fly_script_open_in_executor.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sdalt123/Steal-A-Brainrot-Scripts/main/fly_dont_open_in_executor"))()
