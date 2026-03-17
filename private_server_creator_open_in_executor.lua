4b0cc535c721fc8827d6b1e76c3e911ab372f1ea
Sdalt123 <yeniposta071@outlook.com>
2026-03-17 17:40:52 +0300

    Fix loading script from URL in private server creator

--- private_server_creator_open_in_executor.lua
+++ private_server_creator_open_in_executor.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sdalt123/Steal-A-Brainrot-Scripts/main/private_server_creator_dont_open_in_executor
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sdalt123/Steal-A-Brainrot-Scripts/main/private_server_creator_dont_open_in_executor"))()
+
